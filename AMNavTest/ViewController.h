73e5ff856ee5c563bb8f1566ddb2d9bdd54e3244
PureDesignz <hlaurizi@gmail.com>
2015-06-07 22:50:21 +0200

    Initial Commit

---
+++ AMNavTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMNavTest
+//
+//  Created by Hannes Lauritzen on 07.06.15.
+//  Copyright (c) 2015 PureDesignz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
